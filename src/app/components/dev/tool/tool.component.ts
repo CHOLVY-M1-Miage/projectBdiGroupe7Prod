5f3bee5e34d09a99e39d76c9d52ec57290771956
jordan <ethan.ek23@gmail.com>
2023-01-31 14:46:09 +0100

    teste connection back & front

--- src/app/components/dev/tool/tool.component.ts
+++ src/app/components/dev/tool/tool.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {RouteService} from "../../../service/route.service";
+import {Presentation} from "../../../model/presentation.model";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-tool',
@@ -8,5 +11,16 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   styleUrls: ['./tool.component.css']
 })
 export class ToolComponent {
+  idP = 156;
+  pres = new BehaviorSubject<Presentation[]>([]);
 
+  constructor(private route:RouteService) {
+    this.route.getPresentation(this.idP).subscribe(e => {
+      return this.pres.next(e);
+    })
+  }
+
+  getPresentation():void{
+    this.route.getPresentation(this.idP);
+  }
 }
